8404f0dd6a6732e80fcf6dcff3e36cbe4375f405
Kaili Nishihira <Guest@epicodus-8.local>
2017-10-30 11:14:59 -0700

    this commit will add an mvc application with view and models for sending and receiving messages from twillio

---
+++ Texter/Models/EnvironmentVariables.cs
@@ -0,0 +1,8 @@
+﻿namespace Texter.Models
+{
+	public static class EnvironmentVariables
+	{
+		public static string AccountSid = "AC62b121f58c26107f78d4102b97ca218c";
+		public static string AuthToken = "c04b11526dc5b96bcec7fc5e46ad0251";
+	}
+}
